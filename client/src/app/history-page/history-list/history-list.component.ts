5995d7c5df3c41f96ca01414df7efe2313477d85
Dima Simonov <keldor80@gmail.com>
2018-08-30 13:43:59 +0300

    displaying orders in history page

--- client/src/app/history-page/history-list/history-list.component.ts
+++ client/src/app/history-page/history-list/history-list.component.ts
@@ -1,18 +1,42 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, ViewChild, ElementRef, OnDestroy, AfterViewInit } from '@angular/core';
 
 import { Order } from '../../shared/models/order.model';
+import { MaterialInstance, MaterialService } from '../../shared/services/material.service';
 
 @Component({
   selector: 'crmsc-history-list',
   templateUrl: './history-list.component.html',
   styleUrls: ['./history-list.component.scss']
 })
-export class HistoryListComponent implements OnInit {
+export class HistoryListComponent implements OnDestroy, AfterViewInit {
   @Input() orders: Order[];
+  @ViewChild('modal') modalRef: ElementRef;
+  modal: MaterialInstance;
+  selectedOrder: Order;
 
-  constructor() { }
+  ngOnDestroy() {
+    if (this.modal) {
+      this.modal.destroy();
+    }
+  }
 
-  ngOnInit() {
+  ngAfterViewInit() {
+    this.modal = MaterialService.initModal(this.modalRef);
+  }
+
+  computePrice(order: Order): number {
+    return order.list.reduce((total, item) => {
+      return total += item.quantity * item.cost;
+    }, 0);
+  }
+
+  openOrderDetails(order: Order) {
+    this.selectedOrder = order;
+    this.modal.open();
+  }
+
+  onModalClose() {
+    this.modal.close();
   }
 
 }
