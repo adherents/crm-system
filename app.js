dff8ff3ecb943d2b24d942a697dfd3a616a131e0
Dima Simonov <keldor80@gmail.com>
2018-09-10 12:50:10 +0300

    preparing deployment on heroku server

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const passport = require('passport')
+const path = require('path')
 
 const analyticsRoutes = require('./routes/analytics')
 const authRoutes = require('./routes/auth')
@@ -29,5 +30,17 @@ app.use('/api/analytics', analyticsRoutes)
 app.use('/api/category', categoryRoutes)
 app.use('/api/order', orderRoutes)
 app.use('/api/position', positionRoutes)
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/dist/client'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(
+      path.resolve(
+        __dirname, 'client', 'dist', 'client', 'index.html'
+      )
+    )
+  })
+}
 
 module.exports = app
